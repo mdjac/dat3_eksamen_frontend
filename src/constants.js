14411f430ee475741dea5f54e3ce59e96231cace
Mikkel Dahl Jacobsen <mikkeldahljacobsen@hotmail.com>
2022-01-12 18:06:35 +0100

    final changes

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-//export const URL = "http://localhost:8080/eksamen";
-export const URL = "https://cph-mj838.eu/tomcat/eksamen";
+export const URL = "http://localhost:8080/eksamen";
+//export const URL = "https://cph-mj838.eu/tomcat/eksamen";
