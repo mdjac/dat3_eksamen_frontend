18657b1897856d7f9861d64c4d4299ca4c8b7f00
Mikkel Dahl Jacobsen <mikkeldahljacobsen@hotmail.com>
2022-01-12 13:11:54 +0100

    added DemoScreen

--- src/App.js
+++ src/App.js
@@ -6,56 +6,62 @@ import Nav from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import Home from "./screens/HomeScreen";
 import ProtectedScreen from "./screens/ProtectedScreen";
-import Facade from "./facades/loginFacade";
+import Facade from "./facades/apiFacade";
+import demoFacade from "./facades/demoFacade";
 import AdminScreen from "./screens/AdminScreen";
+import DemoScreen from "./screens/DemoScreen";
 //import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-	const [loggedIn, setLoggedIn] = React.useState(Facade.loggedIn());
-	const [user, setUser] = useState();
-	let history = useHistory();
+  const [loggedIn, setLoggedIn] = React.useState(Facade.loggedIn());
+  const [user, setUser] = useState();
+  let history = useHistory();
 
-	const changeLoginStatus = (pageToGo) => {
-		setLoggedIn(!loggedIn);
-		history.push(pageToGo);
-	};
+  const changeLoginStatus = (pageToGo) => {
+    setLoggedIn(!loggedIn);
+    history.push(pageToGo);
+  };
 
-	useEffect(() => {
-		setUser(Facade.getUser);
-		console.log(user);
-	}, [loggedIn, history]);
+  useEffect(() => {
+    setUser(Facade.getUser);
+    console.log(user);
+  }, [loggedIn, history]);
 
-	return (
-		<div className="App">
-			<Nav
-				loggedIn={loggedIn}
-				user={user}
-				changeLoginStatus={changeLoginStatus}
-			/>
+  return (
+    <div className="App">
+      <Nav
+        loggedIn={loggedIn}
+        user={user}
+        changeLoginStatus={changeLoginStatus}
+      />
 
-			<Switch>
-				<Route exact path="/">
-					<Home user={user} />
-				</Route>
-
-				<PrivateRoute
-					path="/protected"
-					loggedIn={loggedIn}
-					component={ProtectedScreen}
-				/>
-				<PrivateRoute
-					path="/admin"
-					loggedIn={loggedIn}
-					user={user}
-					component={AdminScreen}
-				/>
-
-				<Route path="/login">
-					<Login changeLoginStatus={changeLoginStatus} />
-				</Route>
-			</Switch>
-		</div>
-	);
+      <Switch>
+        <Route exact path="/">
+          <Home user={user} />
+        </Route>
+        <PrivateRoute
+          path="/demo"
+          loggedIn={loggedIn}
+          component={DemoScreen}
+          demoFacade={demoFacade}
+        />
+        <PrivateRoute
+          path="/protected"
+          loggedIn={loggedIn}
+          component={ProtectedScreen}
+        />
+        <PrivateRoute
+          path="/admin"
+          loggedIn={loggedIn}
+          user={user}
+          component={AdminScreen}
+        />
+        <Route path="/login">
+          <Login changeLoginStatus={changeLoginStatus} />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
