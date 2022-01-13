1206b5d7df9f6efd593af6705fb403fcbe832e31
Mikkel Dahl Jacobsen <mikkeldahljacobsen@hotmail.com>
2022-01-13 13:53:26 +0100

    userstory#2 done

--- src/components/DisplayTripsComponent.js
+++ src/components/DisplayTripsComponent.js
@@ -1,10 +1,14 @@
 import examFacade from "../facades/examFacade";
 import Table from "react-bootstrap/Table";
+import { Modal, Container, Row, Col, Button, Form } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 
 const DisplayTripsComponent = () => {
   const [trips, setTrips] = useState([]);
   const [error, setError] = useState();
+
+  //For userstory#2 showing guide details
+  const [modalShow, setModalShow] = useState(false);
 
   useEffect(() => {
     examFacade
@@ -23,7 +27,7 @@ const DisplayTripsComponent = () => {
   }, []);
   return (
     <>
-      <h1>User Story#1 - All Trips</h1>
+      <h1>All Trips</h1>
       {trips.length > 0 ? (
         <>
           <div>
@@ -36,6 +40,7 @@ const DisplayTripsComponent = () => {
                   <td>Location</td>
                   <td>Duration (days)</td>
                   <td>Packing Items</td>
+                  <td>Guide</td>
                 </tr>
               </thead>
               <tbody>
@@ -54,6 +59,26 @@ const DisplayTripsComponent = () => {
                           })}
                         </ul>
                       </td>
+                      <td>
+                        {x.guide.name}
+                        <br></br>
+                        <>
+                          <Button
+                            variant="primary"
+                            size="sm"
+                            onClick={() => {
+                              setModalShow(true);
+                            }}
+                          >
+                            See details {x.guide.name}
+                          </Button>
+                          <MyModal
+                            show={modalShow}
+                            onHide={() => setModalShow(false)}
+                            guide={x.guide}
+                          />
+                        </>
+                      </td>
                     </tr>
                   );
                 })}
@@ -67,6 +92,52 @@ const DisplayTripsComponent = () => {
           <p>Error MSG: {error}</p>
         </>
       )}
+    </>
+  );
+};
+
+const MyModal = (props) => {
+  return (
+    <>
+      <Modal
+        {...props}
+        aria-labelledby="contained-modal-title-vcenter"
+        // centered //Try these 2 settings out
+        // fullscreen
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Guide Details
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <ul key={props.guide.id}>
+              <li> ID: {props.guide.id}</li>
+              <li>Name: {props.guide.name}</li>
+              <li>Birth Year: {props.guide.birthYear}</li>
+              <li>Profile: {props.guide.profile}</li>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: 10,
+                }}
+              >
+                <img
+                  src={props.guide.image}
+                  alt="Failed loading image"
+                  width="200"
+                  height="200"
+                />
+              </div>
+            </ul>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
